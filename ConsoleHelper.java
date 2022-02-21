8c482c72f10b11af95461ecbf9c271a230082ba7
Ibrahim Mohamud Ali <ibrahim2ali169@gmail.com>
2022-02-21 10:17:51 +0000

    Update ConsoleHelper.java

--- ConsoleHelper.java
+++ ConsoleHelper.java
@@ -1,4 +1,43 @@
 package com.codegym.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString(){
+        String s = "";
+
+        while (true){
+            try {
+                s = br.readLine();
+                return s;
+            } catch (IOException io){
+                System.out.println("An error while trying to enter a number. Try again.");
+            }
+        }
+    }
+
+    public static int readInt(){
+        int i = 0;
+
+        while (true){
+            try {
+                i = Integer.parseInt(readString());
+                return i;
+            } catch (NumberFormatException ne) {
+                System.out.println("An error while trying to enter a number. Try again.");
+            }
+        }
+    }
+
+
 }
+
