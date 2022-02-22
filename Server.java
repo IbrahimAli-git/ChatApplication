f9992547fd21d2f69469434d3c9874e857605f10
Ibrahim Mohamud Ali <ibrahim2ali169@gmail.com>
2022-02-22 08:43:02 +0000

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,7 +1,44 @@
 package com.codegym.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    public static void main(String[] args) {
-        
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+
+        public Handler(Socket socket){
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        ConsoleHelper.writeMessage("Please enter port number");
+        ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt());
+        ConsoleHelper.writeMessage("Server is running");
+        Socket socket = null;
+
+
+            while (true) {
+                try {
+                    socket = serverSocket.accept();
+                    new Handler(socket).start();
+
+                } catch (Exception ioException) {
+                    ConsoleHelper.writeMessage(ioException.getMessage());
+
+                    serverSocket.close();
+                    break;
+                }
+            }
     }
 }
